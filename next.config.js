9853be402d7567c96c4cd9a31f8079af08018b49
carlos haro <carlos@MacBook-Air-de-carlos-2.local>
2024-03-15 19:18:01 -0600

    master: config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-};
-
-module.exports = nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    output: 'export',
+  }
+   
+  module.exports = nextConfig
