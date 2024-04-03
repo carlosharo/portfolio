c3a07c05aeabebf6d2c0bb270371a45eb02b56c1
carlos haro <carlos@MacBook-Air-de-carlos-2.local>
2024-04-02 20:53:59 -0600

    master: countdown improvements

--- app/_utils/getFormatTime.tsx
+++ app/_utils/getFormatTime.tsx
@@ -1,10 +1,27 @@
+/**
+ * 
+ * @param time: Time in milliseconds 
+ * @returns: The amount of minutes and seconds transformed from milliseconds
+ */
 export const getTime = (time: number) => {
-    const minutes = `${Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))}`;
-    const seconds = `${Math.floor((time % (1000 * 60)) / 1000)}`;
-    const formatTime = `${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}`
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+    const isRunning = minutes > 0 || seconds > 0;
+    
     return {
         minutes,
         seconds,
-        formatTime: parseInt(minutes) > 0 ? formatTime : '00:00',
+        isRunning,
     }
+}
+
+/**
+ * 
+ * @param minutes: Amount of minutes
+ * @param seconds: Amount of seconds
+ * @returns: The minutes + seconds in the correct format 00:00
+ */
+
+export const getFormat = (minutes: number, seconds: number) =>{
+    return `${minutes}`.padStart(2, '0') + ':' + `${seconds}`.padStart(2, '0');
 }
