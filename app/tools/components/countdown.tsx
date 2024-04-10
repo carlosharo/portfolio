f8e5d8ecde6cd59cf686d844f5402b786dd18924
Carlos Haro <carlos.haro.dev@gmail.com>
2024-04-09 21:56:16 -0600

    portfolio: countdow v2: add stop, add input to chage time

--- app/tools/components/countdown.tsx
+++ app/tools/components/countdown.tsx
@@ -4,27 +4,73 @@ import styles from "../../page.module.css";
 import { getTime, getFormat } from '../../_utils'
 
 export const Countdown = () => {
-    const defaultTime = (45 * 60 * 1000);
     const [counter, setCounter] = useState(0);
-    const { minutes, seconds, isRunning } = getTime(counter);
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const { minutes: currentMinutes, seconds: currentSeconds, isRunning } = getTime(counter);
+    console.log(currentMinutes, currentSeconds, isRunning, counter);
+
+    const handleStart = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        if (isRunning) {
+            setCounter(0);
+        } else {
+            setCounter((minutes * 60 * 1000) + (seconds * 1000));
+        }
+    }
+
+    const handleTime = (e: { target: { value: string; }; }, callFunction: (arg0: number) => void) => {
+        const time = parseInt(e.target.value);
+        callFunction(time >= 0 ? time : 0);
+    }
+
+    const buttonText = isRunning ? 'STOP' : 'START';
 
     useEffect(() => {
-        if (isRunning) {
-            setTimeout(() => {
+        const interval = setInterval(() => {
+            if (isRunning) {
                 setCounter(counter - 1000)
-            }, 1000);
-        }
-    }, [minutes, seconds]);
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [counter]);
 
 
     return (
-        <div key='countdown' className={+minutes === 0 ? '' : +minutes < 15 ? styles.alert : +minutes < 20 ? styles.warning : ''}>
-            <div key='countdow-time' className={styles.countdown}>{isRunning ? getFormat(minutes, seconds) : 'TIME IS UP'}</div>
-            <button disabled={isRunning} className={styles.startbutton} onClick={(e) => {
-                e.preventDefault();
-                setCounter(defaultTime);
-            }}>START</button>
-        </div>
+        <>
+            <div key='countdown' className={+currentMinutes <= 0 && +currentSeconds <= 0 ? '' : +currentMinutes < 15 ? styles.alert : +currentMinutes < 20 ? styles.warning : ''}>
+                <div
+                    key='countdow-time'
+                    className={styles.countdown}
+                >
+                    {isRunning ? getFormat(currentMinutes, currentSeconds) : 'TIME IS UP'}
+                </div>
+                <button
+                    className={styles.startbutton}
+                    onClick={handleStart}
+                >
+                    {buttonText}
+                </button>
+            </div>
+            <div className={styles.countdown_controls}>
+                <label htmlFor="minutes" >Minutes</label>
+                <input
+                    className={styles.countdow_input}
+                    name="minutes" type="number"
+                    value={minutes} onChange={(e) => handleTime(e, setMinutes)}
+                >
+                </input>
+                <label htmlFor="seconds">Seconds</label>
+                <input
+                    className={styles.countdow_input}
+                    name="seconds"
+                    type="number"
+                    value={seconds}
+                    onChange={(e) => handleTime(e, setSeconds)}
+                >
+                </input>
+            </div>
+        </>
 
     );
 }
