a21074cd9bfe7b3f7dcd56515226882091b8310c
Carlos Haro <carlos.haro.dev@gmail.com>
2024-05-27 15:40:04 -0600

    PT-0525: remove highlight

--- app/_components/PTCardDetails/index.tsx
+++ app/_components/PTCardDetails/index.tsx
@@ -4,8 +4,6 @@ import { Box, Grid, IconButton, Link, Stack, Typography } from "@mui/material";
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { AlgorithmsProps } from "@/app/_commons/_types";
 import { useState } from "react";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { qtcreatorDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export const PTCardDetails: React.FC<AlgorithmsProps> = ({ ...algorithm }) => {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -16,7 +14,7 @@ export const PTCardDetails: React.FC<AlgorithmsProps> = ({ ...algorithm }) => {
     }
 
     return (
-        <Box bgcolor='background.paper' sx={{ padding: {md: '0 20px'}, margin: '10px 0', minHeight: '50px' }} >
+        <Box bgcolor='background.paper' sx={{ padding: { md: '0 20px' }, margin: '10px 0', minHeight: '50px' }} >
             <Stack direction='row' justifyContent='space-between' alignItems='center'>
                 <Link href={href} underline='none' color='secondary'>
                     <Typography variant='subtitle1'>{title}</Typography>
@@ -26,7 +24,7 @@ export const PTCardDetails: React.FC<AlgorithmsProps> = ({ ...algorithm }) => {
                 </IconButton>
             </Stack>
             {isExpanded && (
-                <PTCardContent sx={{padding:'0'}}>
+                <PTCardContent sx={{ padding: '0' }}>
                     <Box bgcolor="background.default" sx={{ minWidth: '100%', padding: '10px' }} >
                         <Typography variant='subtitle2' sx={{ whiteSpace: 'preserve-breaks' }}>{subtitle}</Typography>
                     </Box>
@@ -42,14 +40,7 @@ export const PTCardDetails: React.FC<AlgorithmsProps> = ({ ...algorithm }) => {
                     <Grid container bgcolor='background.default' justifyContent='center'>
                         <Grid item xs={12} md={6}>
                             <Typography variant='caption' sx={{ fontSize: { xs: '8px', sm: '10px', md: '12px' } }}>
-                                <SyntaxHighlighter 
-                                    language="javascript"
-                                    showLineNumbers
-                                    style={qtcreatorDark}
-                                    codeTagProps={{ style: { color: '#bde2ff' } }}
-                                >
-                                    {`${example.code}`}
-                                </SyntaxHighlighter>
+                                {`${example.code}`}
                             </Typography>
                         </Grid>
                         <Grid item xs={12} md={6} bgcolor='background.paper'>
